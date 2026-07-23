4335961d581d3c3011ab1cf8d45a2a86abd90b55
jhilomhaldar <jhilomhaldar.india@gmail.com>
2026-07-23 18:13:20 +0530

    Add deployment check endpoint

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import health, auth_test, oauth, instructions, roles, users, focus_company_intelligence, focus_accounts, focus_account_intelligence, focus_contacts, focus_report, leadforms, campaigns, client_apiusage, accounts, contacts, deals, activities, notes, attachments
+from app.api.v1.endpoints import health, auth_test, oauth, instructions, roles, users, focus_company_intelligence, focus_accounts, focus_account_intelligence, focus_contacts, focus_report, leadforms, campaigns, client_apiusage, accounts, contacts, deals, activities, notes, attachments, deploy_check
 
 
 api_router = APIRouter()
@@ -103,4 +103,9 @@ api_router.include_router(
 api_router.include_router(
     attachments.router,
     tags=["Attachments"]
+)
+
+api_router.include_router(
+    deploy_check.router,
+    tags=["Deploy Check"]
 )
